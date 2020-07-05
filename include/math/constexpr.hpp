da1a96d6dc266fd9a2a303b53d4c65f58486756e
StellaSmith <stellasmith3@outlook.com>
2020-07-04 23:49:10 -0400

    Implemented c_ilog10 and c_ilog2

--- include/math/constexpr.hpp
+++ include/math/constexpr.hpp
@@ -1,5 +1,5 @@
 #ifndef MATH_CONSTEXPR_HPP
-#define MATH_CONSTRXPR_HPP
+#define MATH_CONSTEXPR_HPP
 
 namespace math
 {
@@ -25,20 +25,87 @@ namespace math
     template <std::int64_t v, std::uint64_t x>
     inline constexpr std::int64_t c_ipow_v = c_ipow<v, x>::value;
 
-    // constexpr integer logarithm
-    template <std::int64_t v, std::uint64_t x>
-    struct c_ilog
+    // constexpr integer logarithm (rounded down)
+    template <std::uint64_t x>
+    struct c_ilog10
+    {
+        constexpr static std::uint64_t value = 1 + c_ilog10<x / 10>::value;
+    };
+
+    template <>
+    struct c_ilog10<9>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<8>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<7>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<6>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<5>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<4>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<3>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+    template <>
+    struct c_ilog10<2>
+    {
+        constexpr static std::uint64_t value = 1;
+    };
+
+    template <>
+    struct c_ilog10<1>
+    {
+        constexpr static std::uint64_t value = 0;
+    };
+
+    template <>
+    struct c_ilog10<0>
     {
     };
 
-    template <std::int64_t v, std::uint64_t x>
-    inline constexpr std::int64_t c_ilog_v = c_ilog<v, x>::value;
+    template <std::uint64_t x>
+    struct c_ilog2
+    {
+        constexpr static std::uint64_t value = 1 + c_ilog2<x / 2>::value;
+    };
 
-    template <std::int64_t v>
-    inline constexpr std::int64_t c_ilog10_v = c_ilog<v, 10>::value;
+    template <>
+    struct c_ilog2<1>
+    {
+        constexpr static std::uint64_t value = 0;
+    };
 
-    template <std::int64_t v>
-    inline constexpr std::int64_t c_ilog2_v = c_ilog<v, 2>::value;
+    template <>
+    struct c_ilog2<0>
+    {
+    };
+
+    template <std::uint64_t x>
+    inline constexpr std::uint64_t c_ilog10_v = c_ilog10<x>::value;
+
+    template <std::uint64_t x>
+    inline constexpr std::uint64_t c_ilog2_v = c_ilog2<x>::value;
 
 } // namespace math
 
