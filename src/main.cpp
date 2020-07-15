309766c99eebcf7cb0e8c87cdf4706802601b999
StellaSmith <stellasmith3@outlook.com>
2020-07-15 03:04:29 -0400

    Added support for configuration files and enabled Wayland & KMSDRM

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "engine/chunk_renderer.hpp"
 #include "engine/chunk_t.hpp"
 #include "engine/game.hpp"
+#include "Config.hpp"
 
 #include <glad/glad.h>
 #include <SDL.h>
@@ -9,20 +10,115 @@
 #include <cstdlib>
 #include <cstdio>
 #include <string>
+#include <charconv>
+
+static SDL_NORETURN void show_error(std::string_view msg, SDL_Window *w = nullptr)
+{
+    if (SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR!", msg.data(), w) < 0)
+        SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "%s", msg.data());
+    std::exit(EXIT_FAILURE);
+}
+
+Config config_engine;
+
+using namespace std::literals;
 
 int main(int argc, char **argv)
 {
     constexpr int width = 640, height = 480;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+
+    for (int i = 0; i < argc; ++i)
     {
-        SDL_Log("SDL Error: %s", SDL_GetError());
-        std::exit(EXIT_FAILURE);
+        if (argv[i] == "--sdl-video-drivers"sv)
+        {
+            int drivers = SDL_GetNumVideoDrivers();
+            std::printf("Available video drivers (%d):\n", drivers);
+            for (int i = 0; i < drivers; ++i)
+                std::printf("\t%d) %s\n", i + 1, SDL_GetVideoDriver(i));
+        }
+        else if (argv[i] == "--sdl-audio-drivers"sv)
+        {
+            int drivers = SDL_GetNumAudioDrivers();
+            std::printf("Available audio drivers (%d):\n", drivers);
+            for (int i = 0; i < drivers; ++i)
+                std::printf("\t%d) %s\n", i + 1, SDL_GetAudioDriver(i));
+        }
     }
+
+    if (SDL_Init(0) < 0)
+        show_error("Error initializing SDL: "s + SDL_GetError());
+
+    {
+        char const *fname = "./cfg/engine.cfg";
+        FILE *fp = std::fopen(fname, "r");
+        if (!fp)
+            show_error("Error opening engine configuration file. ("s + fname + ")");
+
+        try
+        {
+            config_engine = Config::from_file(fp);
+        }
+        catch (std::exception &e)
+        {
+            std::fclose(fp);
+            if (SDL_ShowSimpleMessageBox(SDL_MessageBoxFlags::SDL_MESSAGEBOX_ERROR, "EXCEPTION!", e.what(), nullptr) < 0)
+                SDL_LogCritical(SDL_LOG_CATEGORY_SYSTEM, "%s", e.what());
+
+            throw;
+        }
+        std::fclose(fp);
+    }
+    {
+        auto video_driver = config_engine.get_or("sdl_video_driver"sv, {});
+        if (SDL_VideoInit(video_driver.empty() ? nullptr : video_driver.data()) < 0)
+            show_error("Error initializing SDL Video subsystem: "s + SDL_GetError());
+    }
+    {
+        auto audio_driver = config_engine.get_or("sdl_audio_driver"sv, {});
+        if (SDL_AudioInit(audio_driver.empty() ? nullptr : audio_driver.data()) < 0)
+            show_error("Error initializing SDL Audio subsystem: "s + SDL_GetError());
+    }
+
+    auto get_integer = [](std::string_view v, unsigned &i) {
+        auto o_integer = config_engine.get(v);
+        if (o_integer.has_value())
+        {
+            auto [ptr, ec] = std::from_chars(o_integer->data(), o_integer->data() + o_integer->size(), i);
+            if (ec != std::errc{})
+            {
+                std::string error_str;
+                error_str += v;
+                error_str += " is not an unsigned integer"sv;
+                show_error(error_str);
+            }
+        }
+    };
+
+    unsigned red_bits = 3, green_bits = 3, blue_bits = 2, alpha_bits = 0, depth_bits = 24, stencil_bits = 0;
+    get_integer("sdl_gl_red_size"sv, red_bits);
+    get_integer("sdl_gl_green_size"sv, green_bits);
+    get_integer("sdl_gl_blue_size"sv, blue_bits);
+    get_integer("sdl_gl_alpha_size"sv, alpha_bits);
+    get_integer("sdl_gl_depth_size"sv, depth_bits);
+    get_integer("sdl_gl_stencil_size"sv, stencil_bits);
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+    if (SDL_GL_SetAttribute(SDL_GL_RED_SIZE, red_bits) < 0)
+        show_error("Error setting SDL_GL_RED_SIZE");
+    if (SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, green_bits) < 0)
+        show_error("Error setting SDL_GL_GREEN_SIZE");
+    if (SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, blue_bits) < 0)
+        show_error("Error setting SDL_GL_BLUE_SIZE");
+    if (SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, alpha_bits) < 0)
+        show_error("Error setting SDL_GL_ALPHA_SIZE");
+    if (SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, depth_bits) < 0)
+        show_error("Error setting SDL_GL_DEPTH_SIZE");
+    if (SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, stencil_bits) < 0)
+        show_error("Error setting SDL_GL_STENCIL_SIZE");
+
 #ifndef NDEBUG
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
 #endif
@@ -33,31 +129,19 @@ int main(int argc, char **argv)
         /*Size (width, height)*/ width, height,
         /*Flags*/ SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
-    if (window == nullptr)
-    {
-        SDL_Log("SDL Error: %s", SDL_GetError());
-        std::exit(EXIT_FAILURE);
-    }
+    if (!window)
+        show_error("Can't create main window: "s + SDL_GetError());
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 
-    if (gl_context == nullptr)
-    {
-        SDL_Log("SDL Error: %s", SDL_GetError());
-        std::exit(EXIT_FAILURE);
-    }
+    if (!gl_context)
+        show_error("Can't create OpenGL 3.3 context: "s + SDL_GetError(), window);
 
     if (SDL_GL_MakeCurrent(window, gl_context) < 0)
-    {
-        SDL_Log("SDL Error: %s", SDL_GetError());
-        std::exit(EXIT_FAILURE);
-    }
+        show_error("Can't set OpenGL context current: "s + SDL_GetError(), window);
 
     if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
-    {
-        SDL_Log("GLAD Error: Failed to initialize the OpenGL context.");
-        std::exit(EXIT_FAILURE);
-    }
+        show_error("GLAD Error: Failed to initialize the OpenGL context.", window);
 
     {
         int major, minor, r, g, b, a, d, s;
@@ -77,10 +161,6 @@ int main(int argc, char **argv)
 
         std::printf("OpenGL %d.%d\n\tRGBA bits: %d, %d, %d, %d\n\tDepth bits: %d\n\tStencil bits: %d\n", major, minor, r, g, b, a, d, s);
         std::printf("\tVersion: %s\n\tVendor: %s\n\tRenderer: %s\n\tShading language version: %s\n", version, vendor, renderer, shading_version);
-
-        GLint max_vertex_attribs;
-        glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &max_vertex_attribs);
-        std::printf("\tMax vertex attribs: %d\n", max_vertex_attribs);
 
         if (SDL_GL_ExtensionSupported("GL_KHR_debug"))
         {
@@ -126,6 +206,8 @@ int main(int argc, char **argv)
 
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
+    SDL_AudioQuit();
+    SDL_VideoQuit();
     SDL_Quit();
 
     return 0;
