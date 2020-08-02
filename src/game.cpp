ecb4d1770098b17c736a11dbf0833f3a6870fd48
StellaSmith <stellasmith3@outlook.com>
2020-08-01 23:44:24 -0400

    Window size change handling

--- src/game.cpp
+++ src/game.cpp
@@ -141,6 +141,11 @@ void engine::Game::input(SDL_Event const &event)
 {
     if (event.type == SDL_QUIT)
         stop();
+    else if (event.type == SDL_WINDOWEVENT) {
+        if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+            glViewport(0, 0, event.window.data1, event.window.data2);
+        }
+    }
 }
 
 void engine::Game::update(engine::Game::clock_type::duration delta)
