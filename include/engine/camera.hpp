051cafc74ee57bd9fc4df0332c8af179904c0030
StellaSmith <stellasmith3@outlook.com>
2020-10-01 23:52:11 -0300

    Camera movement and fixed mipmaps

--- include/engine/camera.hpp
+++ include/engine/camera.hpp
@@ -1,9 +1,11 @@
 #ifndef ENGINE_CAMERA_HPP
 #define ENGINE_CAMERA_HPP
 
+#include <glm/vec3.hpp>
+
 namespace engine {
     struct Camera {
-        glm::vec3 position = glm::vec3 { 0.0f, 0.0f, -1.0f };
+        glm::vec3 position = glm::vec3 { -8.0f, 0.0f, -8.0f };
         glm::vec3 forward = glm::vec3 { 0.0f, 0.0f, 1.0f };
         glm::vec3 up = glm::vec3 { 0.0f, 1.0f, 0.0f };
         float fov = 60.0f;
