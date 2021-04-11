8db70629c6e36dbcdba85a79536f3be565eea4d4
StellaSmith <stellasmith3@outlook.com>
2021-04-11 18:03:44 -0400

    Added mouse speed multiplier

--- src/game/update.cpp
+++ src/game/update.cpp
@@ -32,6 +32,7 @@ static auto get_sorted_indices(engine::rendering::Mesh const &mesh)
 
 int g_render_distance_horizontal = 12;
 int g_render_distance_vertical = 4;
+float g_mouse_sensitivity = 1;
 
 #include <cstring>
 #include <tuple>
@@ -59,6 +60,7 @@ void engine::Game::update([[maybe_unused]] engine::Game::clock_type::duration de
 
     if (ImGui::Begin("Camera")) {
         ImGui::SliderFloat("FOV", &g_camera.fov, 30.0f, 130.0f);
+        ImGui::SliderFloat("Mouse speed", &g_mouse_sensitivity, 0.1f, 10.0f);
         ImGui::SliderFloat("Near", &g_camera.near, 0.01, 1000.0f);
         ImGui::SliderFloat("Far", &g_camera.far, 0.01, 1000.0f);
         ImGui::Text("Position: % .5f % .5f % .5f", g_camera.position.x, g_camera.position.y, g_camera.position.z);
