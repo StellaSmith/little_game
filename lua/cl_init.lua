bfacde481e44e3475b4ea3867e221df4be961d6f
StellaSmith <stellasmith3@outlook.com>
2021-08-25 20:56:38 -0400

    [CI] Add mingw

--- lua/cl_init.lua
+++ lua/cl_init.lua
@@ -1,5 +1,41 @@
 print "Initializing client side"
 
-local my_tbl = setmetatable({}, {__tostring = function(self) return "owo" end})
+local StaticBlockType = {}
 
-print("my_tbl:", my_tbl)
+function StaticBlockType:GetModel()
+    return { model=self.model, textures=self.textures }
+end
+
+function PreInitialize()
+    -- request the engine to load some required assets at startup
+    game.RequestAsset("models/base/CommonBlock.json")
+    game.RequestAsset("textures/white.png")
+    game.RequestAsset("textures/minecraft/programmer_art/stone.png")
+    game.RequestAsset("textures/minecraft/programmer_art/dirt.png")
+    game.RequestAsset("textures/minecraft/programmer_art/dirt.png")
+end
+
+local function Transform(func, array)
+    local result = {}
+    for k, v in pairs(array) do
+        result[k] = func(v)
+    end
+    return result
+end
+
+local function RegisterStaticBlock(name, model, ...)
+    local textures = {...}
+    local new_block_type = {model=model,textures=textures}
+    setmetatable(new_block_type,{__index=StaticBlockType})
+    return game.RegisterBlock(name, new_block_type)
+end
+
+function Initialize()
+
+    
+
+    StoneBlockType.model = game.GetAsset("models/base/CommonBlock.json")
+    StoneBlockType.textures = {
+        game.GetAsset("textures/minecraft/programmer_art/stone.png")
+    }
+end
