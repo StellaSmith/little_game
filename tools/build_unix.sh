1c9b401a503f9fef6751866c5d01d999ddaff7e1
StellaSmith <stellasmith3@outlook.com>
2022-04-17 18:14:09 -0400

    Use gnu++20

--- tools/build_unix.sh
+++ tools/build_unix.sh
@@ -9,7 +9,7 @@ if [ ! -e build/.tools_timestamp ] || [ -n "$(find tools/ -newer build/.tools_ti
 fi
 
 if [ ! -e build/.install_timestamp ] || [ -n "$(find recipes/vgame/ -newer build/.install_timestamp -print -quit)" ]; then
-    conan install recipes/vgame/ vgame/latest@ -if build/ -of build/ -u -b missing -s compiler.cppstd=20
+    conan install recipes/vgame/ vgame/latest@ -if build/ -of build/ -u -b missing -s compiler.cppstd=gnu20
     touch -m build/.install_timestamp
 fi
 
