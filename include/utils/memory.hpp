754e988aa666a56d47435de6cc10c72cead6c736
StellaSmith <stellasmith3@outlook.com>
2022-06-01 15:31:38 -0400

    Add helper to (de)allocate objects using allocators

--- include/utils/memory.hpp
+++ include/utils/memory.hpp
@@ -25,6 +25,26 @@ namespace utils {
         return std::unique_ptr<void, VoidDeleter> { ::operator new (bytes, std::align_val_t { alignment }) };
     }
 
+    template <typename Alloc, typename... Args>
+    auto new_object(Alloc &allocator, Args &&...args) -> typename std::allocator_traits<Alloc>::pointer
+    {
+        auto ptr = std::allocator_traits<Alloc>::allocate(allocator, 1);
+        try {
+            std::allocator_traits<Alloc>::construct(allocator, ptr, std::forward<Args>(args)...);
+        } catch (...) {
+            std::allocator_traits<Alloc>::deallocate(allocator, ptr, 1);
+            throw;
+        }
+        return ptr;
+    }
+
+    template <typename Alloc>
+    void delete_object(Alloc &allocator, typename std::allocator_traits<Alloc>::pointer ptr) noexcept
+    {
+        std::allocator_traits<Alloc>::destroy(allocator, ptr);
+        std::allocator_traits<Alloc>::deallocate(allocator, ptr, 1);
+    }
+
 }
 
 #endif
