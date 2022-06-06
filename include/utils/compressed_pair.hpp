34cdd281658490f16632404ac4069cee439c61e2
StellaSmith <stellasmith3@outlook.com>
2022-06-06 19:14:22 -0400

    tuple_element, tuple_size & get for compressed_pair

--- include/utils/compressed_pair.hpp
+++ include/utils/compressed_pair.hpp
@@ -37,4 +37,58 @@ namespace utils {
     };
 }
 
+#include <utility>
+
+namespace std {
+    template <typename First, typename Second>
+    struct tuple_element<0, utils::compressed_pair<First, Second>> {
+        using type = First;
+    };
+
+    template <typename First, typename Second>
+    struct tuple_element<1, utils::compressed_pair<First, Second>> {
+        using type = Second;
+    };
+
+    template <typename First, typename Second>
+    struct tuple_size<utils::compressed_pair<First, Second>> : std::integral_constant<std::size_t, 2> {
+    };
+}
+
+namespace utils {
+    namespace impl {
+
+        template <typename T>
+        struct is_compressed_pair : std::false_type {
+        };
+
+        template <typename First, typename Second>
+        struct is_compressed_pair<utils::compressed_pair<First, Second>> : std::true_type {
+        };
+
+    }
+
+    template <std::size_t I, typename T>
+    requires(
+        (impl::is_compressed_pair<std::remove_cvref_t<T>>::value)
+        && I == 0
+        && !std::is_empty_v<typename T::first_type>)
+
+        decltype(auto) get(T &&pair) noexcept
+    {
+        return static_cast<T &&>(pair).first;
+    }
+
+    template <std::size_t I, typename T>
+    requires(
+        (impl::is_compressed_pair<std::remove_cvref_t<T>>::value)
+        && I == 1
+        && !std::is_empty_v<typename T::second_type>)
+
+        decltype(auto) get(T &&pair) noexcept
+    {
+        return static_cast<T &&>(pair).second;
+    }
+}
+
 #endif
