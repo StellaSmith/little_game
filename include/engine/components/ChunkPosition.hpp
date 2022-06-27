b1deb5e4bb7cd0b963d4b741beb167d2c9b4df78
StellaSmith <stellasmith3@outlook.com>
2022-06-27 14:37:33 -0400

    Add hashing and operator== to C_ChunkPosition

--- include/engine/components/ChunkPosition.hpp
+++ include/engine/components/ChunkPosition.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <glm/vec4.hpp>
 
 namespace engine::components {
 
@@ -10,14 +9,35 @@ namespace engine::components {
         std::int32_t y {};
         std::int32_t z {};
         std::int32_t dimension {};
-        constexpr operator glm::i32vec4() const noexcept
-        {
-            return { x, y, z, dimension };
-        }
     };
 
+    constexpr bool operator==(ChunkPosition const &lhs, ChunkPosition const &rhs) noexcept
+    {
+        return lhs.dimension == rhs.dimension && lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
+    }
+
+    constexpr bool operator!=(ChunkPosition const &lhs, ChunkPosition const &rhs) noexcept
+    {
+        return lhs.dimension != rhs.dimension || lhs.x != rhs.x || lhs.y != rhs.y || lhs.z != rhs.z;
+    }
 } // namespace engine::components
 
 namespace engine {
     using C_ChunkPosition = engine::components::ChunkPosition;
 } // namespace engine
+
+#include <boost/container_hash/hash.hpp>
+#include <functional>
+
+namespace std {
+
+    template <>
+    struct hash<engine::C_ChunkPosition> {
+        std::size_t operator()(engine::C_ChunkPosition const &position) const noexcept
+        {
+            std::int32_t const arr[] = { position.x, position.y, position.z, position.dimension };
+            return boost::hash_value(arr);
+        }
+    };
+
+}
