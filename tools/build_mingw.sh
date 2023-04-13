9b9443cf46315a1e9c77c7515950f139e44433de
Stella Smith <stellasmith3@outlook.com>
2023-04-13 16:25:19 -0400

    simplify build scripts

--- tools/build_mingw.sh
+++ tools/build_mingw.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex -o pipefail
+
 ###
 # Feel free to modify this file to use the proper environment for your case
 ###
@@ -10,29 +12,22 @@ os=Windows
 toolchain=/usr/${arch}-w64-mingw32
 target_host=${arch}-w64-mingw32
 build_type=Release
+
 cc=${target_host}-cc
 cxx=${target_host}-c++
-
-###
-# Build tools
-###
-set -e
-mkdir -p build/cross/${target_host}/
-
-if [ ! -e build/vgame_tools/.timestamp ] || [ -n "$(find recipes/vgame_tools tools/ -newer build/vgame_tools/.timestamp -print -quit)" ]; then
-    echo "#### exporting tools ####"
-    conan install -if build/vgame_tools -b missing recipes/vgame_tools/ vgame_tools/latest@ -pr:b default -pr:h default -s:h compiler.cppstd=20 -s:b compiler.cppstd=20 &&
-    conan export -l  build/vgame_tools/conan.lock recipes/vgame_tools/ vgame_tools/latest@ &&
-    touch -m build/vgame_tools/.timestamp
-fi
-
-set +e
+as=${target_host}-as
+ar=${target_host}-ar
+ranlib=${target_host}-ranlib
+strip=${target_host}-strip
+windres=${target_host}-windres
 
 GCC_INFO=$(${cc} --verbose 2>&1)
-GCC_VERSION=$(sed -nr 's/^gcc version ([0-9]+\.[0-9]+).*?/\1/p' <<< ${GCC_INFO})
-THREAD_MODEL=$(sed -nr 's/^Thread model: (.*)$/\1/p' <<< ${GCC_INFO})
+GCC_VERSION=$(echo "${GCC_INFO}" | sed -nr 's/^gcc version ([0-9]+\.[0-9]+).*?/\1/p')
+THREAD_MODEL=$(echo "${GCC_INFO}" | sed -nr 's/^Thread model: (.*)$/\1/p')
 
-cat <<< "[settings]
+mkdir -p build/cross/${target_host}
+cat >build/cross/${target_host}/profile <<EOF
+[settings]
 build_type=${build_type}
 os=${os}
 arch=${arch}
@@ -42,41 +37,30 @@ compiler.cppstd=gnu20
 compiler.libcxx=libstdc++11
 compiler.threads=${THREAD_MODEL}
 compiler.exception=seh
+[conf]
+tools.build:sysroot=
+tools.cmake.cmaketoolchain:system_name=${os}
+tools.cmake.cmaketoolchain:system_processor=${arch}
+tools.build:compiler_executables={"c": "${cc}", "cxx": "${cxx}", "asm": "${as}", "rc": "${windres}"}
 [options]
-boost:i18n_backend_iconv=libiconv
-boost:without_stacktrace=True
-[env]
-CONAN_BASH_PATH=$(realpath $(command -v sh))
-CONAN_CMAKE_SYSROOT=${toolchain}
-CONAN_CMAKE_FIND_ROOT_PATH=${toolchain}
-CONAN_CMAKE_SYSTEM_NAME=${os}
-CONAN_CMAKE_SYSTEM_PROCESSOR=${arch}
-CHOST=$target_host
-AR=$target_host-ar
-AS=$target_host-as
-RANLIB=$target_host-ranlib
+boost/*:i18n_backend_iconv=libiconv
+boost/*:without_stacktrace=True
+[buildenv]
+CHOST=${target_host}
+AR=${ar}
+AS=${as}
+RANLIB=${ranlib}
 CC=${cc}
 CXX=${cxx}
-STRIP=$target_host-strip
-RC=$target_host-windres
-CXXFLAGS=-fext-numeric-literals
-LDFLAGS=-Wl,-push-state -Wl,-Bstatic,--whole-archive -Wl,-as-needed -static-libgcc -static-libstdc++ $([ "${THREAD_MODEL}" = "posix" ] && echo "-lwinpthread") -Wl,-pop-state
-" > build/cross/${target_host}/profile
+STRIP=${strip}
+RC=${windres}
+EOF
+# LDFLAGS=-Wl,-push-state -Wl,-Bstatic,--whole-archive -Wl,-as-needed -static-libgcc -static-libstdc++ $([ "${THREAD_MODEL}" = "posix" ] && echo "-lwinpthread") -Wl,-pop-state
+# CXXFLAGS=-fext-numeric-literals
 
 # statically link against libgcc, libstdc++, and libwinpthread
 
 echo Using mingw toolchain at ${toolchain} 1>&2
 echo Targeting ${target_host} 1>&2
 
-
-set -e
-if [ ! -e build/cross/${target_host}/.timestamp ] || [ -n "$(find . ! -path './.git/*' ! -exec git check-ignore -q {} ';' -newer build/cross/${target_host}/.timestamp -print -quit)" ]; then
-    echo "#### building game ####"
-    conan install recipes/vgame/ vgame/latest@ -if build/cross/${target_host}/ -pr:b default -s:b compiler.cppstd=gnu20 -pr:h build/cross/${target_host}/profile -b missing
-    conan build recipes/vgame/ -bf build/cross/${target_host}/
-    touch -m build/cross/${target_host}/.timestamp
-fi
-
-echo "#### updating compile_commands.json ####"
-rm -f build/compile_commands.json;
-ln -s $(realpath build/cross/${target_host}/compile_commands.json) build/
+exec conan build . -pr:b default -s:b compiler.cppstd=gnu20 -pr:h build/cross/${target_host}/profile -b missing
--- tools/build_unix.sh
+++ tools/build_unix.sh
@@ -1,22 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e -o pipefail
-
-mkdir -p build/vgame{_tools,}
-
-if [ ! -e build/vgame_tools/.timestamp ] || ! conan list -c "vgame_tools" | grep ERROR >/dev/null 2>&1 || [ -n "$(find tools/ -newer build/vgame_tools/.timestamp -print -quit)" ]; then
-    echo "#### exporting tools ####"
-    conan export --build-require recipes/vgame_tools/ --version none
-    touch -m build/vgame_tools/.timestamp
-fi
-
-if [ ! -e build/vgame/.timestamp ] || [ -n "$(find ./ ! -path './.git/*' ! -exec git check-ignore -q {} ';' -newer build/vgame/.timestamp -print -quit)" ]; then
-    echo "#### building game ####"
-    conan install -u recipes/vgame/ --version none -b missing -of build/vgame -pr:b default -pr:h default -s build_type=Debug
-    conan build recipes/vgame/ -of build/vgame
-    touch -m build/vgame/.timestamp
-fi
-
-echo "#### updating compile_commands.json ####"
-rm -f build/compile_commands.json
-ln -s "$(realpath build/vgame/compile_commands.json)" build/
+exec conan build . -pr:h default -s:h compiler.cppstd=20 -b missing
